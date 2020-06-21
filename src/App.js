512edd857190654f1541bfecc50b6735e7525cea
Pedro Almeida <59484474+PedroMarianoAlmeida@users.noreply.github.com>
2020-06-21 14:49:29 -0300

    Basic Structure
    All necessary components to start the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route} from "react-router-dom";
+
+import HomePage from './components/HomePage';
+import Header from './components/Header-and-Footer/Header';
+import Footer from './components/Header-and-Footer/Footer';
+
+import Nationality from './components/Nationality';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+
+      <BrowserRouter>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/nationality" component={Nationality} />
+      </BrowserRouter>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Nationality.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Nationality = () => {
+    return (
+        <h1>Nationality</h1>
+      );
+}
+ 
+export default Nationality;
